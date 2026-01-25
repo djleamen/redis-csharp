de2e69558bf6a8bd4e39dc60e0fad05a6be8d8cf
DJ <dj.leamen@ontariotechu.net>
2026-01-24 21:15:03 -0500

    pong

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,27 @@
+/**
+ * Redis - A simple Redis server implementation in C#
+ * From CodeCrafters.io build-your-own-redis (C#)
+ */
+
 using System.Net;
 using System.Net.Sockets;
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+while (true)
+{
+    Socket client = server.AcceptSocket(); // wait for client
+    
+    // Read incoming 
+    byte[] buffer = new byte[1024];
+    client.Receive(buffer);
+    
+    // Send PONG response
+    string response = "+PONG\r\n";
+    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+    client.Send(responseBytes);
+    
+    client.Close();
+}
