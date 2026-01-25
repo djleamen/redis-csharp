24686da8eaa084b61aa56c126401e478159901fd
DJ <dj.leamen@ontariotechu.net>
2026-01-24 21:16:20 -0500

    multiple pings

--- src/Server.cs
+++ src/Server.cs
@@ -14,14 +14,29 @@ while (true)
 {
     Socket client = server.AcceptSocket(); // wait for client
     
-    // Read incoming 
-    byte[] buffer = new byte[1024];
-    client.Receive(buffer);
-    
-    // Send PONG response
-    string response = "+PONG\r\n";
-    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-    client.Send(responseBytes);
+    // Handle multiple commands on the same connection
+    while (true)
+    {
+        try
+        {
+            // Read incoming command
+            byte[] buffer = new byte[1024];
+            int bytesRead = client.Receive(buffer);
+            
+            // Client disconnected
+            if (bytesRead == 0)
+                break;
+            
+            // Send PONG response
+            string response = "+PONG\r\n";
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            client.Send(responseBytes);
+        }
+        catch
+        {
+            break;
+        }
+    }
     
     client.Close();
 }
